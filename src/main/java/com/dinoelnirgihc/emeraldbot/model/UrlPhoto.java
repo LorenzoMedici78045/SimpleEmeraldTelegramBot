2dfb4c1cf81ca31ed0054bc75f79499230a80c11
Dinoel Nirgihc <susakim@mail.ru>
2025-02-13 20:52:09 +0300

    Исправил проблему с отображением фотографий

--- src/main/java/com/dinoelnirgihc/emeraldbot/model/UrlPhoto.java
+++ src/main/java/com/dinoelnirgihc/emeraldbot/model/UrlPhoto.java
@@ -2,8 +2,8 @@ package com.dinoelnirgihc.emeraldbot.model;
 
 public abstract class UrlPhoto
 {
-    public final static String  ORIGINPH = "https://ya.ru/images/search?from=tabbar&img_url=https%3A%2F%2Fvsegda-pomnim.com%2Fuploads%2Fposts%2F2023-03%2F1678974856_vsegda-pomnim-com-p-berillii-kamen-foto-55.jpg&lr=39&p=1&pos=30&rpt=simage&text=%D0%B8%D0%B7%D1%83%D0%BC%D1%80%D1%83%D0%B4";
-    public final static String  FIELDSPH = "https://ya.ru/images/search?from=tabbar&img_url=https%3A%2F%2Frussianindustry.com%2Fuploads%2Fp%2Ffg%2F863%2F!p2010033.jpg&lr=39&pos=16&rpt=simage&text=%D0%B4%D0%BE%D0%B1%D1%8B%D1%87%D0%B0%20%D0%B8%D0%B7%D1%83%D0%BC%D1%80%D1%83%D0%B4%D0%BE%D0%B2";
-    public final static String  CUTTINGPH = "https://ya.ru/images/search?from=tabbar&img_url=https%3A%2F%2Fsouthklad.ru%2Fforum%2Fpic%2F124973.jpg&lr=39&pos=0&rpt=simage&text=%D0%BE%D0%B3%D1%80%D0%B0%D0%BD%D0%BA%D0%B0%20%D0%B8%D0%B7%D1%83%D0%BC%D1%80%D1%83%D0%B4%D0%BE%D0%B2%20%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81";
-    public final static String  SYNTHETICPH = "https://ya.ru/images/search?from=tabbar&img_url=https%3A%2F%2Fultrafiolet.guru%2Fimage%2Fcatalog%2Fultrafioletovye-fonari%2Fmineralogija%2Fstati%2Frubin%2Fproverka-rubina-ultrafioletom.jpg&lr=39&p=1&pos=23&rpt=simage&text=%D0%BE%D1%82%D0%BB%D0%B8%D1%87%D0%B8%D0%B5%20%D0%B8%D1%81%D0%BA%D1%83%D1%81%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D0%B3%D0%BE%20%D0%B8%D0%B7%D1%83%D0%BC%D1%80%D1%83%D0%B4%D0%B0%20%D0%BE%D1%82%20%D0%BD%D0%B0%D1%82%D1%83%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE";
+    public final static String  ORIGINPH = "https://cloud.mail.ru/public/7Qxd/PTYc8Wf2E";
+    public final static String  FIELDSPH = "https://cloud.mail.ru/public/PkER/f4wLe4WtM";
+    public final static String  CUTTINGPH = "https://cloud.mail.ru/public/Rajd/heJ7yvmse";
+    public final static String  SYNTHETICPH = "https://cloud.mail.ru/public/F32B/dFq5Hsi8T";
 }
